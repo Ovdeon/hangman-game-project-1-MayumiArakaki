7c3d991d6ca5881e6d929434861beb8044d4a942
Mayumi Arakaki <110319704+MayumiArakaki@users.noreply.github.com>
2023-02-13 22:35:01 -0500

    Hangman

--- main.py
+++ main.py
@@ -1,1 +1,103 @@
-# code here
+import json
+import random
+
+with open("words.json") as data:
+    list = json.load(data)
+# print(list["data"])
+
+def randomWord ():
+    aleatorio = random.choice(list["data"])
+    return aleatorio
+
+def printWord(word,letterChoisen):
+    l = len(word)
+    letters = []
+    for i in range(l):
+        if word[i] in letterChoisen:
+            letters.append(word[i])
+        else:
+            letters.append("_")
+    return letters
+
+def WinCondition(word):
+    if "_" in word:
+        return False
+    return True
+
+def drawHangman(array,error):
+    print(array[error])    
+
+hangMan = ['''
+      +---+
+      |   |
+          |
+          |
+          |
+          |
+    =========''', '''
+      +---+
+      |   |
+      O   |
+          |
+          |
+          |
+    =========''', '''
+      +---+
+      |   |
+      O   |
+      |   |
+          |
+          |
+    =========''', '''
+      +---+
+      |   |
+      O   |
+     /|   |
+          |
+          |
+    =========''', '''
+      +---+
+      |   |
+      O   |
+     /|\  |
+          |
+          |
+    =========''', '''
+      +---+
+      |   |
+      O   |
+     /|\  |
+     /    |
+          |
+    =========''', '''
+      +---+
+      |   |
+      O   |
+     /|\  |
+     / \  |
+          |
+    =========''']
+
+chosenWord = str(randomWord())
+print(chosenWord)
+errors = 0
+chosenLetter = []
+a = printWord(chosenWord,chosenLetter)
+print(a)
+
+while errors <= 5:
+    x = input("Ingrese la letra: ")
+    if not(x.isnumeric) or not(x.isalpha) or (x in chosenLetter) or not(x in chosenWord):
+        chosenLetter.append(x)
+        errors = errors + 1
+        print("Haz perdido una oportunidad, te quedan " + str(6-errors))
+        drawHangman(hangMan,errors)
+        if errors == 6:
+            print("Haz perdido")
+    elif x in chosenWord:
+        chosenLetter.append(x)
+        if WinCondition(printWord(chosenWord,chosenLetter)):
+            print(printWord(chosenWord,chosenLetter))
+            print("Felicidades Has ganado")
+            break
+        print(printWord(chosenWord,chosenLetter))
